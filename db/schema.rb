a977b4bd3484c6088b638f9c99b12e1c4c9af510
Dan Coughlin <dan.coughlin@gmail.com>
2015-04-07 18:28:11 -0400

    updated database schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150404172900) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
